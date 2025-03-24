d74c96861ac5a9570493e5b413ece581b00df741
booz avila <tessersoft@gmail.com>
2025-03-23 20:02:08 -0700

    feat: Agregar nuevas plantillas y patrones al tema 'kiosko'; eliminar archivos del tema 'boutique'

--- wp-content/themes/shop/index.php
+++ wp-content/themes/shop/index.php
@@ -1,8 +1,11 @@
 <?php
 get_header();
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-get_template_part( 'entry' );
-comments_template();
-endwhile; endif;
-get_template_part( 'nav', 'below' );
+
+if (is_front_page() && is_home()) { // Solo en la homepage por defecto
+    if (function_exists('is_plugin_active') && is_plugin_active('custom-plugin/custom-plugin.php')) {
+        echo do_shortcode('[banner_carousel]');
+    }
+}
+
+get_template_part('nav', 'below');
 get_footer();
